f1e47ecfd6c5891669595fd206d3939c75b05945
Bill Morris <wboykinm@geosprocket.com>
2020-11-02 10:13:57 -0500

    day 2

--- 2020/config.sh
+++ 2020/config.sh
@@ -8,7 +8,7 @@ unzip ne_10m_admin_1_states_provinces.zip
 # Fire up a Postgres DB and enable PostGIS
 dropdb maptember_2020 --if-exists
 createdb maptember_2020
-psql maptember_2020 -c "CREATE EXTENSION postgis;"
+psql maptember_2020 -c 'CREATE EXTENSION IF NOT EXISTS postgis;CREATE EXTENSION IF NOT EXISTS "uuid-ossp";'
 
 # Move the data to the new DB (to VT state-plane, because *sigh*)
 ogr2ogr -where "name_en like 'Vermont%'" -t_srs "EPSG:32145" -f "PostgreSQL" PG:"host=localhost dbname=maptember_2020" ne_10m_admin_1_states_provinces.shp -nln vt_border
@@ -28,13 +28,59 @@ psql maptember_2020 -c "
         d.*
       FROM (
         SELECT
-          (ST_DumpPoints(wkb_geometry)).geom AS the_geom
+          (ST_DumpPoints(wkb_geometry)).geom AS the_geom_32145
         FROM vt_border
       ) d
     )
     SELECT
+      uuid_generate_v4() AS id,
       random() * 5 AS size,
-      ST_translate(the_geom,(random() * 10000) - 5000,(random() * 10000) - 5000) AS the_geom
+      ST_translate(the_geom_32145,(random() * 10000) - 5000,(random() * 10000) - 5000) AS the_geom_32145
     FROM points
+  )
+"
+
+######################################################################
+# DAY 2: LINES
+######################################################################
+# Note that I'm living dangerously without indexes b/c this is a small dataset. 
+# I bet it'll become necessary later on down the road . . .
+
+# Continuing with the geonoise theme,
+# let's join all the day 1 points to their closest neighbor
+# (https://gis.stackexchange.com/questions/302290/spatial-join-attributes-based-on-closest-point-in-postgis)
+# then make lines to connect them
+
+psql maptember_2020 -c "
+  DROP TABLE IF EXISTS day2;
+  CREATE TABLE day2 AS (
+    SELECT
+      uuid_generate_v4() AS id,
+      ST_Makeline(a.the_geom_32145,c.the_geom_32145) AS the_geom_32145
+    FROM day1 a
+    JOIN LATERAL (
+      SELECT 
+        the_geom_32145
+      FROM day1 b
+      WHERE a.id != b.id
+      ORDER BY a.the_geom_32145 <-> b.the_geom_32145
+      LIMIT 1
+    ) AS c ON true 
+
+    -- For the hell of it, let's try to sunburst this thing,
+    -- with lines radiating out from the state centroid
+    UNION ALL
+
+    SELECT
+      uuid_generate_v4() AS id,
+      ST_Makeline(
+        the_geom_32145,
+        (
+          SELECT
+            ST_Centroid(wkb_geometry)
+          FROM vt_border
+        )
+      ) AS the_geom_32145
+    FROM day1
   )
 "
