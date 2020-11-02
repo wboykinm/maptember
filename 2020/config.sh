f2c63d8775936a3c3754e9d36a74fc5493ae6256
Bill Morris <wboykinm@geosprocket.com>
2020-11-01 20:57:47 -0500

    day 1!

---
+++ 2020/config.sh
@@ -0,0 +1,40 @@
+# Let's grab some basedata - Vermont placenames!
+wget -c https://opendata.arcgis.com/datasets/ee1280c35bc7435b890288e50b55a8a8_3.csv?outSR=%7B%22latestWkid%22%3A32145%2C%22wkid%22%3A32145%7D -O vt_placenames.csv
+
+# . . . and a bunch of state/province boundaries!
+wget -c https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip -O ne_10m_admin_1_states_provinces.zip
+unzip ne_10m_admin_1_states_provinces.zip
+
+# Fire up a Postgres DB and enable PostGIS
+dropdb maptember_2020 --if-exists
+createdb maptember_2020
+psql maptember_2020 -c "CREATE EXTENSION postgis;"
+
+# Move the data to the new DB (to VT state-plane, because *sigh*)
+ogr2ogr -where "name_en like 'Vermont%'" -t_srs "EPSG:32145" -f "PostgreSQL" PG:"host=localhost dbname=maptember_2020" ne_10m_admin_1_states_provinces.shp -nln vt_border
+
+######################################################################
+# DAY 1: POINTS
+######################################################################
+
+# Let's create a layer that explodes the state boundary to points, 
+# then perturbs them up to 10km,
+# and randomly-sizes them
+psql maptember_2020 -c "
+  DROP TABLE IF EXISTS day1;
+  CREATE TABLE day1 AS (
+    WITH points AS (
+      SELECT
+        d.*
+      FROM (
+        SELECT
+          (ST_DumpPoints(wkb_geometry)).geom AS the_geom
+        FROM vt_border
+      ) d
+    )
+    SELECT
+      random() * 5 AS size,
+      ST_translate(the_geom,(random() * 10000) - 5000,(random() * 10000) - 5000) AS the_geom
+    FROM points
+  )
+"
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # maptember
-30 day map challenge repo
+30 day map challenge! (Bill's entries)
+
+![day1](img/day1.png)
+
+Data processing methods and images associated with the ever-popular [30-day map challenge](https://www.gislounge.com/november-map-challenge/).
