237c02f2608ac54e49a54e45786ca5a21078d6ac
Bill Morris <wboykinm@geosprocket.com>
2020-11-03 17:30:50 -0500

    day4

--- 2020/config.sh
+++ 2020/config.sh
@@ -1,5 +1,4 @@
-# Let's grab some basedata - Vermont placenames!
-wget -c https://opendata.arcgis.com/datasets/ee1280c35bc7435b890288e50b55a8a8_3.csv?outSR=%7B%22latestWkid%22%3A32145%2C%22wkid%22%3A32145%7D -O vt_placenames.csv
+# MAPTEMBER YO
 
 # . . . and a bunch of state/province boundaries!
 wget -c https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_1_states_provinces.zip -O ne_10m_admin_1_states_provinces.zip
@@ -145,5 +144,94 @@ psql maptember_2020 -c "
         (SELECT ST_Buffer(wkb_geometry,1000) FROM vt_border)
       ) AS the_geom_32145
     FROM v
+  )
+"
+
+######################################################################
+# DAY 4: HEXAGONS
+######################################################################
+
+# Everyone's hipster favorite!
+# Let's throw a grid over the state of Vermont, then aggregate some interesting values to the cells.
+
+# First order of business - add some appropriate functions, adapted from work provided by the good folks at Carto:
+# https://github.com/CartoDB/cartodb-postgresql/blob/362af5e6a0792ce65e8a842cdc1c0dd36d6da6ad/scripts-available/CDB_Hexagon.sql
+psql maptember_2020 -f lib/cdb_functions.sql
+
+# Then grab moar basedata - Vermont placenames!
+wget -c https://opendata.arcgis.com/datasets/ee1280c35bc7435b890288e50b55a8a8_3.csv?outSR=%7B%22latestWkid%22%3A32145%2C%22wkid%22%3A32145%7D -O vt_placenames.csv
+
+# . . . and import/add geometry
+psql maptember_2020 -c "
+  DROP TABLE IF EXISTS vt_placenames;
+  CREATE TABLE vt_placenames (
+    x float,
+    y float,
+    objectid text,
+    gnisid text,
+    gname text,
+    gntype text,
+    cntyname text,
+    quadname text,
+    townname text,
+    cntygeoid int,
+    prim_lat_dec float,
+    prim_long_dec float,
+    elev_in_ft int,
+    date_created text,
+    date_edited text
+  );
+"
+psql maptember_2020 -c "\COPY vt_placenames FROM 'vt_placenames.csv' CSV HEADER;
+  SELECT AddGeometryColumn ('public','vt_placenames','the_geom_32145',32145,'GEOMETRY',2);
+  UPDATE vt_placenames 
+  SET the_geom_32145 = ST_Transform(
+    ST_GeomFromText(
+      'POINT(' || prim_long_dec || ' ' || prim_lat_dec || ')',
+      4326
+    ),
+    32145
+  );
+"
+
+# generate 1.5km hexgrid and assign values to cells
+psql maptember_2020 -c "
+  DROP TABLE IF EXISTS day4;
+  CREATE TABLE day4 AS (
+    -- add a grid over the bbox of the state
+    WITH grid AS (
+      SELECT
+        CDB_HexagonGrid(wkb_geometry,1500) AS the_geom_32145
+      FROM vt_border
+    ),
+    -- grab just the cells that overlap the state polygon
+    vt_grid AS (
+      SELECT
+        *
+      FROM grid
+      WHERE ST_Intersects(
+        the_geom_32145,
+        (SELECT ST_Buffer(wkb_geometry,1000) FROM vt_border)
+      )
+    )
+    -- aggregate some stats from placename points
+    SELECT
+      g.the_geom_32145,
+      count(p.gnisid) AS count_features,
+      -- Set to elevation of the nearest known point where there
+      -- are no contained reference points (I know, this is lazy)
+      COALESCE(
+        avg(p.elev_in_ft),
+        (
+          SELECT 
+            p2.elev_in_ft
+          FROM vt_placenames p2
+          ORDER BY g.the_geom_32145 <-> p2.the_geom_32145
+          LIMIT 1
+        )
+      ) AS avg_elev
+    FROM vt_grid g
+    LEFT JOIN vt_placenames p ON ST_Intersects(g.the_geom_32145,p.the_geom_32145)
+    GROUP BY g.the_geom_32145
   )
 "
